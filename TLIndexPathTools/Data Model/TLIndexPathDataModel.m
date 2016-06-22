6cac2fd70956f797061eb335a96d63a055fa7c3e
David Troy <dave@410labs.com>
2016-06-22 14:46:25 -0400

    no fixed identifier

--- TLIndexPathTools/Data Model/TLIndexPathDataModel.m
+++ TLIndexPathTools/Data Model/TLIndexPathDataModel.m
@@ -301,7 +301,7 @@ const NSString *TLIndexPathDataModelNilSectionName = @"__TLIndexPathDataModelNil
 
 + (id)identifierForItem:(id)item identifierBlock:(id (^)(id))identifierBlock
 {
-    id identifier = [item valueForKey:@"identifier"];
+    id identifier;
     if (identifierBlock) {
         @try {
             identifier = identifierBlock(item);
